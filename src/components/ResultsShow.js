0b49cf213999e50f708416e031f8b753a4d327cb
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-03-11 22:30:44 +0000

    push

--- src/components/ResultsShow.js
+++ src/components/ResultsShow.js
@@ -9,15 +9,15 @@ const ResultsShow = (props) => {
 
   useEffect(() => {
     if (score >= 3) {
-      setMessage(`Ouch... ${name}...`)
+      setMessage(`Ouch... ${name}... try again?`)
     }
   
     if (score > 3 && score <= 6) {
-      setMessage(`Not bad ${name}, better luck next time`)
+      setMessage(`Not bad ${name}, but better luck next time...`)
     }
   
     if (score > 7) {
-      setMessage(`Nice one ${name} :)`)
+      setMessage(`Nice one ${name}! :)`)
     }
   
     if (score === 10) {
@@ -30,10 +30,12 @@ const ResultsShow = (props) => {
 
   return (
     <div className="wrapper">
-      <h1>Results</h1>
-      <p>Score: {score} </p>
-      <p>{message}</p>
-      <Link className="button" to="/">PLAY AGAIN</Link>
+      <div className="ui-wrapper">
+        <h1>Results</h1>
+        <p>Score: {score} </p>
+        <p>{message}</p>
+        <Link className="button" to="/">PLAY AGAIN</Link>
+      </div>
     </div>
   )
 }
