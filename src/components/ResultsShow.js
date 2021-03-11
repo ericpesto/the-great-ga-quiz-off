045da6079ebd0e871d682c4aea749d2ffc6446b9
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-03-11 21:19:34 +0000

    personalised message

--- src/components/ResultsShow.js
+++ src/components/ResultsShow.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 
 const ResultsShow = (props) => {
+  const [message, setMessage] = useState('')
   console.log(props)
   const score = props.score
   const name = props.location.state
 
+  useEffect(() => {
+    if (score >= 3) {
+      setMessage(`Ouch. Better luck next time ${name}`)
+    }
+  
+    if (score > 3 && score <= 6) {
+      setMessage(`Not bad ${name}, but also not great`)
+    }
+  
+    if (score > 7) {
+      setMessage(`Nice one ${name}`)
+    }
+  
+    if (score === 10) {
+      setMessage(`Wow ${name}, nerd status`)
+    }
+  }, [])
+  
 
-  // ! DISPLAY SCORE
-  // ! DISPLAY PERSONALISED COMMENT
-  // ! PLAY AGAIN BUTTON
+  console.log('message', message)
+
+
+
+  // ! DISPLAY PERSONALISED COMMENT BASED On SCORE
+
 
   // const displayScore = () => {
   // //   // let startScore = 0
@@ -35,7 +57,7 @@ const ResultsShow = (props) => {
     <>
       <h1>Results</h1>
       <p>Score: {score} </p>
-      <p>PERSONALISED MESSAGE {name}</p>
+      <p>{message}</p>
       <Link to="/">PLAY AGAIN</Link>
     </>
   )
