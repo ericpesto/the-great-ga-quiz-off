31eb2bd687634693e146e9a2da35a0d45ddaeb63
Eric Petsopoulos <epetsopoulos@gmail.com>
2021-03-11 21:21:43 +0000

    push

--- src/components/ResultsShow.js
+++ src/components/ResultsShow.js
@@ -28,31 +28,6 @@ const ResultsShow = (props) => {
 
   console.log('message', message)
 
-
-
-  // ! DISPLAY PERSONALISED COMMENT BASED On SCORE
-
-
-  // const displayScore = () => {
-  // //   // let startScore = 0
-  // //   // startScore += 1
-    
-
-  // }
-  // displayScore()
-
-  // personalizedComment = () => {
-  //   if (scoreCount === 1, 2, 3) {
-  //     return '{name}, you definitely need more practice, better chance next time'
-  //   } else if (scoreCount === 4, 5, 6, 7) {
-  //     return '{name}, Your ok…just ok lol'
-  //   } else {
-  //     return 
-  //       '{name}, you are the bomb!!'
-      
-  //   }
-  // }
-
   return (
     <>
       <h1>Results</h1>
